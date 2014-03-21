d96733c547d1e1b1875deb51e0cc1f59373ede33
Jeri Wieringa <jeri.elizabeth@gmail.com>
2014-03-21 10:58:43 -0400

    move to mobile first style; updated through link colors

--- common/header.php
+++ common/header.php
@@ -38,11 +38,11 @@
 
             <?php echo theme_header_image(); ?>
 
+            <div id="site-title"><?php echo link_to_home_page(theme_logo()); ?></div>
+
             <div id="search-container">
                 <?php echo search_form(); ?>
             </div><!-- end search -->
-
-            <div id="site-title"><?php echo link_to_home_page(theme_logo()); ?></div>
             
             <nav id="top-nav">
                 <?php echo public_nav_main(); ?>
