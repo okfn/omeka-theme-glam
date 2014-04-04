5573a7f7bca85fda4560d179fc0642d8463424b8
Jeri Wieringa <jeri.elizabeth@gmail.com>
2014-04-04 18:10:52 -0400

    getting navigation to a basic level

--- common/header.php
+++ common/header.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <?php if ( $description = option('description')): ?>
     <meta name="description" content="<?php echo $description; ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php endif; ?>
     <?php
     if (isset($title)) {
