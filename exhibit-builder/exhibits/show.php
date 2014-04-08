8fc92719cf87dc7a8294d5686248ab7623aade2d
Jeri Wieringa <jeri.elizabeth@gmail.com>
2014-04-08 15:27:44 -0400

    exhibit styles with two columns in show

---
+++ exhibit-builder/exhibits/show.php
@@ -0,0 +1,40 @@
+<?php
+echo head(array(
+    'title' => metadata('exhibit_page', 'title') . ' &middot; ' . metadata('exhibit', 'title'),
+    'bodyclass' => 'exhibits show'));
+?>
+
+<nav id="exhibit-pages">
+    <?php echo exhibit_builder_page_nav(); ?>
+</nav>
+
+
+<div id="primary">
+    <h1><span class="exhibit-page"><?php echo metadata('exhibit_page', 'title'); ?></h1>
+
+    <?php if (count(exhibit_builder_child_pages(get_current_record('exhibit_page'))) > 0): ?>
+    <nav id="exhibit-child-pages">
+        <?php echo exhibit_builder_child_page_nav(); ?>
+    </nav>
+    <?php endif; ?>
+    
+    <?php exhibit_builder_render_exhibit_page(); ?>
+    
+    <div id="exhibit-page-navigation">
+        <?php if ($prevLink = exhibit_builder_link_to_previous_page()): ?>
+        <div id="exhibit-nav-prev">
+        <?php echo $prevLink; ?>
+        </div>
+        <?php endif; ?>
+        <?php if ($nextLink = exhibit_builder_link_to_next_page()): ?>
+        <div id="exhibit-nav-next">
+        <?php echo $nextLink; ?>
+        </div>
+        <?php endif; ?>
+        <div id="exhibit-nav-up">
+        <?php echo exhibit_builder_page_trail(); ?>
+        </div>
+    </div>
+</div>
+
+<?php echo foot(); ?>
